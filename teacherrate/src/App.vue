fc61aaa154fad3ed21c707a8eca99c1f1bd8efbd
markojakob <markjakobo@gmail.com>
2025-09-24 15:26:45 +0300

    Front end: Vue is installed. Added navbar example. Todo: Design navbar

---
+++ teacherrate/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <MainMenu />
+    <router-view />
+  </div>
+</template>
+
+<script>
+import MainMenu from "./components/MainMenu.vue";
+
+export default {
+  name: "App",
+  components: {
+    MainMenu
+  }
+};
+</script>
+
+<style>
+
+</style>
